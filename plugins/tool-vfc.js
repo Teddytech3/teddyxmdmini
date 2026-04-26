a70af39ea98f03e9d3cae7a9f4637ee4ca092eb8
TEDDY TECH <kibetwycliffe093@gmail.com>
2026-04-27 00:23:05 +0300

    Swap imports for config and inconnuboy modules

--- plugins/tool-vfc.js
+++ plugins/tool-vfc.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const config = require('../inconnuboy')
-const { cmd, commands } = require('../command')
+const config = require('../config')
+const { cmd, commands } = require('../inconnuboy')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 
 
